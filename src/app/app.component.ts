78282e88dbe7620a6d4eb151d8b599040621b0c8
lempurdysh <ryuote@gmail.com>
2023-12-21 23:08:46 +0200

    feature: implemented avatar component

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,20 +3,56 @@ import { CommonModule } from '@angular/common';
 import { RouterOutlet } from '@angular/router';
 import { ButtonComponent } from './shared/components/button/button.component';
 import { ButtonType } from './shared/components/button/button-type.enum';
+import { AvatarComponent } from './shared/components/avatar/avatar.component';
+import { BoxConstraint } from './shared/box-sizing';
 
 @Component({
   selector: 'app-root',
   standalone: true,
   template: `
     <router-outlet></router-outlet>
-    <mfc-button [buttonType]="buttonType.Primary">button</mfc-button>
-    <mfc-button [buttonType]="buttonType.Secondary">button</mfc-button>
-    <mfc-button [buttonType]="buttonType.Accent">button</mfc-button>
-    <mfc-button [buttonType]="buttonType.Positive">button</mfc-button>
-    <mfc-button [buttonType]="buttonType.Error">button</mfc-button>
+    <mfc-avatar
+      src="https://cdn.fotofits.com/petzlover/gallery/img/l/yorkshire-terrier-698831.jpg"
+      alt="puppy!!"
+    />
+    <mfc-avatar
+      src="https://cdn.fotofits.com/petzlover/gallery/img/l/yorkshire-terrier-698831.jpg"
+      alt="puppy!!"
+      sizeType="xs"
+    />
+    <mfc-avatar
+      src="https://cdn.fotofits.com/petzlover/gallery/img/l/yorkshire-terrier-698831.jpg"
+      alt="puppy!!"
+      sizeType="sm"
+    />
+    <mfc-avatar
+      src="https://cdn.fotofits.com/petzlover/gallery/img/l/yorkshire-terrier-698831.jpg"
+      alt="puppy!!"
+      sizeType="md"
+    />
+    <mfc-avatar
+      src="https://cdn.fotofits.com/petzlover/gallery/img/l/yorkshire-terrier-698831.jpg"
+      alt="puppy!!"
+      sizeType="lg"
+    />
+    <mfc-avatar
+      src="https://cdn.fotofits.com/petzlover/gallery/img/l/yorkshire-terrier-698831.jpg"
+      alt="puppy!!"
+      sizeType="xl"
+    />
+    <mfc-avatar
+      src="https://cdn.fotofits.com/petzlover/gallery/img/l/yorkshire-terrier-698831.jpg"
+      alt="puppy!!"
+      [size]="customSize"
+    />
   `,
-  imports: [CommonModule, RouterOutlet, ButtonComponent],
+  imports: [CommonModule, RouterOutlet, ButtonComponent, AvatarComponent],
 })
 export class AppComponent {
   public readonly buttonType = ButtonType;
+
+  public readonly customSize: BoxConstraint = {
+    width: '512px',
+    height: '512px',
+  };
 }
--- src/app/shared/components/avatar/avatar.component.spec.ts
+++ src/app/shared/components/avatar/avatar.component.spec.ts
@@ -2,16 +2,16 @@ import { ComponentFixture, TestBed } from '@angular/core/testing';
 
 import { AvatarComponent } from './avatar.component';
 
+// Why even create tests for this?
 describe('AvatarComponent', () => {
   let component: AvatarComponent;
   let fixture: ComponentFixture<AvatarComponent>;
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      imports: [AvatarComponent]
-    })
-    .compileComponents();
-    
+      imports: [AvatarComponent],
+    }).compileComponents();
+
     fixture = TestBed.createComponent(AvatarComponent);
     component = fixture.componentInstance;
     fixture.detectChanges();
